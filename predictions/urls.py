0e26be24395fc3ef2d20a0bdae3966b775370280
Priyal Singh <priyalrajput595@gmail.com>
2026-03-01 22:01:51 +0530

    Added the path url to newsfeed

--- predictions/urls.py
+++ predictions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import predict_crop, prediction_history
 from . import views
+from .news_views import agro_news
 
 urlpatterns = [
     path("predict/", predict_crop, name="predict"),
@@ -8,6 +9,6 @@ urlpatterns = [
     path("insights/", views.user_insights, name="insights"),
     path("history/<int:pk>/", views.delete_prediction, name="delete_prediction"),
     path("history/reset/", views.reset_history, name="reset_history"),
-
+    path("news/", agro_news, name="agro_news"),
 
 ]
