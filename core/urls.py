0549fc0c99e63d3a6249d34677606499063bf1c8
Priyal Singh <priyalrajput595@gmail.com>
2026-03-03 22:32:02 +0530

    removed merge conflicts

--- core/urls.py
+++ core/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     path("", include("accounts.urls")),
     path("api/", include("predictions.urls")),
     path("api/", include("weather.urls")), 
-<<<<<<< HEAD
     path('learn/', include('learning.urls')),
-=======
-    path("api/", include("assistant.urls")),
     path("api/chatbot/", include("guided_chatbot.urls")),
->>>>>>> 115ee52 (added rule-based guided chatbot)
 ]
